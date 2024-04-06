356e91cae71a6791a2dfeddc370ea25a93fffa80
yothin tangchanachaiphong <yothin.tangchanachaiphong@gmail.com>
2024-04-06 20:50:58 +0700

    ติดต่อกับฐานข้อมูล

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nest-crud',
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true,
+  }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
